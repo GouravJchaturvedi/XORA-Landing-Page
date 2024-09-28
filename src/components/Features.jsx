3470c77655f9248576d7db216549a7ba526c0584
Gourav Chaturvedi <chaturvedigourav47@gmail.com>
2024-09-28 19:04:43 +0530

    features'

---
+++ src/components/Features.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Element } from 'react-scroll'
+
+function Features() {
+  return (
+    <div>
+        <Element name='features'>
+            <div className='container'>
+                <div className='relative flex'>
+                    
+                </div>
+            </div>
+        </Element>
+    </div>
+  )
+}
+
+export default Features
